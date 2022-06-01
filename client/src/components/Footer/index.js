265c2370675c3847b3a19ae8686884f6d0efd43d
Nicole Barranca <nikkibarranca@gmail.com>
2022-05-31 21:10:45 -0400

    Footer and Header components basic

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>HI</div>;
+  return (
+    <nav>
+      <h2>WhateverNow</h2>
+      <ul>
+        <li>Home</li>
+        <li>Account Settings</li>
+        <li>Login</li>
+        <li>Sign Up</li>
+        <li>Logout</li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Footer;
