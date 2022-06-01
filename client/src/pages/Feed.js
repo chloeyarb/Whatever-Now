edc41b89ff924ef5ecfdb6c09f381c64f11ef7f9
Nicole Barranca <nikkibarranca@gmail.com>
2022-06-01 13:21:47 -0400

    feed component styling

--- client/src/pages/Feed.js
+++ client/src/pages/Feed.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 const Feed = () => {
   return (
     <div className="container mt-5 mb-5">
       <div className="row">
         <div className="col-md-12 col-sm-12">
-          <h1 className="opacity-25 mb-3">Whatever...</h1>
+          <h1 className="opacity-25 mb-3">
+            What<span className="fw-light">ever</span>...
+          </h1>
         </div>
       </div>
 
@@ -21,11 +24,41 @@ const Feed = () => {
         <Button
           variant="dark"
           type="submit"
-          className="w-25 fs-5 fw-bold mt-3 button-color"
+          className="w-25 fs-5 fw-bold mt-3 mb-5 button-color"
         >
           Publish Post
         </Button>
       </Form>
+
+      <>
+        {/* Card with image top, text bottom */}
+        <Card className="mb-3 w-75 ">
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="dark" type="submit" className="">
+              Like
+            </Button>
+          </Card.Body>
+        </Card>
+        <br />
+        {/* Card with text top, image bottom */}
+        <Card className="mb-3 w-75">
+          <Card.Body>
+            <Button variant="dark" type="submit" className="">
+              Like
+            </Button>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+          <Card.Img variant="bottom" src="holder.js/100px180" />
+        </Card>
+      </>
     </div>
   );
 };
