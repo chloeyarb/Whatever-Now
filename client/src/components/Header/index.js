e9f4612d2c2ec0f24de97bfb9211ae2498c93283
Nicole Barranca <nikkibarranca@gmail.com>
2022-05-31 20:29:23 -0400

    Header and Footer components initial setup

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav>
+      <h2>WhateverNow</h2>
+      <ul>
+        <li>Home</li>
+        <li>Account Settings</li>
+        <li>Login</li>
+        <li>Sign Up</li>
+        <li>Logout</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Header;
