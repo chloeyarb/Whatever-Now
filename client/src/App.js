1d32b0104f268a9ac3478f7db200cb2c4bce231b
Nicole Barranca <nikkibarranca@gmail.com>
2022-06-01 20:35:23 -0400

    implemented react-router

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,19 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Feed from "./pages/Feed";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
-      <Feed />
+      <div>
+        <Routes>
+          <Route path="/" element={<Feed />} />
+        </Routes>
+      </div>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
