8ee969e4db6ea18d33d9e11163ff2e8278415f5f
Cherboi <rplatzer98@gmail.com>
2022-06-01 20:09:22 -0400

    account page

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Feed from "./pages/Feed";
+import Account from "./pages/Account";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div>
       <Header />
+      <Account />
       <Feed />
       <Footer />
     </div>
