82647adbc61b83547fc106d2a8e1a8b5db76a81a
Cherboi <rplatzer98@gmail.com>
2022-06-02 09:53:24 -0400

    account page

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,13 @@ import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Feed from "./pages/Feed";
-import Account from "./pages/Account";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div>
       <Header />
-      <Account />
       <Feed />
       <Footer />
     </div>
--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -1,8 +1,22 @@
 import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 
+
+
+// need to render the page with and acoounts settings with change PFP, change password, and add birthday
 const Account = () => {
-
+    return (
+        <div className="container mt-5 mb-5">
+            <div className="row">
+                <h1 className="mb-3">
+                    Account settings
+                </h1>
+            </div>
+        </div>
+    );
 }
+
 
 export default Account
