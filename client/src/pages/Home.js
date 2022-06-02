132c1ac15d22fc506cf0271f18654e2b791dc5d7
Nicole Barranca <nikkibarranca@gmail.com>
2022-06-02 15:28:14 -0400

    Homepage styling and links is footer

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,12 +3,12 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, ThemeProvider } from "react-bootstrap";
 const Home = () => {
   return (
     <Container fluid="md">
       <Card className="bg-dark text-white">
-        <Card.Img src="/img/HI.png" alt="Card image" />
+        <Card.Img src="/img/HI.png" alt="Card image" className="food" />
         <Card.ImgOverlay>
           {/* STYLE WELCOME MESSAGE + ADD BUTTONS */}
           <Card.Text className="hero-text">
@@ -18,34 +18,11 @@ const Home = () => {
           <Card.Text>Last updated 3 mins ago</Card.Text>
         </Card.ImgOverlay>
       </Card>
-      <Row className="justify-content-md-center">
-        <Col md="auto">
-          Welcome to Whatever Now
-          <div>
-            <img
-              src="/img/HI.png"
-              className="img-fluid"
-              style={{ maxWidth: "80rem" }}
-              alt="heropic"
-            />
-          </div>
-          {/* <div>Are mission statement being that Media its-self has a week self- life at best, with that being because of the overabundance of news or that People’s attention span is not that long. So why not have a social media Page that reflex that. With our app “What-3ver Now” that is our goal to achieve. With a little thing where every post and convo for that week is burned after. The week starts Sunday at 12est and ends Sunday 12est. Not ever post will last a week full week. Now it might see like we are going after a “Free-Speech” app as many others have done with their disdain for how Twitter or others have there Policys for posting content, but I assure you here we do intend to “watch posts” to see if any nefarious or malicious posts or comments are poste
-            </div> */}
-          <div className="mb-2">
-            <Button variant="primary" size="lg">
-              Login
-            </Button>{" "}
-            <Button variant="primary" size="lg">
-              Sign Up
-            </Button>
-          </div>
-        </Col>
-      </Row>
 
       <div className="container mt-5 mb-5">
         <div className="row">
           <div className="col-md-12 col-sm-12">
-            <h1 className="opacity-25 mb-3">
+            <h1 className="opacity-25 mb-3 text-light">
               What<span className="fw-light">ever</span>...
             </h1>
           </div>
@@ -63,9 +40,9 @@ const Home = () => {
           </Form.Group>
 
           <Button
-            variant="dark"
+            variant="warning"
             type="submit"
-            className="w-25 fs-5 fw-bold mt-3 mb-5 button-color"
+            className="w-25 fs-5 fw-bold mt-3 mb-5 "
           >
             Publish Post
           </Button>
@@ -87,7 +64,7 @@ const Home = () => {
           </Card>
           <br />
           {/* Card with text top, image bottom */}
-          <Card className="mb-3 w-75">
+          {/* <Card className="mb-3 w-75">
             <Card.Body>
               <Button variant="dark" type="submit" className="">
                 Like
@@ -98,7 +75,7 @@ const Home = () => {
               </Card.Text>
             </Card.Body>
             <Card.Img variant="bottom" src="holder.js/100px180" />
-          </Card>
+          </Card> */}
         </>
       </div>
     </Container>
