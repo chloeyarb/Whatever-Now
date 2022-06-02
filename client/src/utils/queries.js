fb34c775abd815bbd00b108c90d586737213590b
Chloe Yarborough <cmyarboro@gmail.com>
2022-06-01 23:38:55 -0400

    added mutations

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,44 +1,44 @@
 import { gql } from '@apollo/client';
 
 export const QUERY_POSTS = gql`
-query posts($username: String) {
-    posts(username: $username) {
-        _id
-        username
-        postText
-        createdAt
-        likes
-        likeCount
-        comments {
+    query posts($username: String) {
+        posts(username: $username) {
             _id
             username
-            commentBody
+            postText
             createdAt
-        }
+            likes
+            likeCount
+            comments {
+                _id
+                username
+                commentBody
+                createdAt
+            }
     }
 }
 `;
 // should we use ID instead of username? 
 export const QUERY_POST = gql`
-query post($username: String!) {
-    post(username: $username) {
-        _id
-        username
-        postText
-        createdAt
-        likes
-        likeCount 
-        comments {
+    query post($username: String!) {
+        post(username: $username) {
             _id
             username
-            commentBody
+            postText
             createdAt
-        }
+            likes
+            likeCount 
+            comments {
+                _id
+                username
+                commentBody
+                createdAt
+            }
     }
 }
 `;
 
-export const QUERY_USER =gql`
+export const QUERY_USER = gql`
 query user($username: String!) {
     user(username: $username) {
         _id
@@ -50,8 +50,28 @@ query user($username: String!) {
             createdAt
             likeCount
         }
-        likedPosts {
+        likedPosts
+    }
+}`;
+
+export const QUERY_ME = gql`
+{
+    me {
+        _id
+        username
+        likedPosts
+        posts {
             _id
+            postText
+            createdAt
+            likes
+            likeCount
+            comments {
+                _id
+                username
+                commentBody
+                createdAt
+            }
         }
     }
-}`
+}`;
