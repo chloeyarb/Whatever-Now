65e7f0c2395b5d67e30cb68141b1ff6465247f43
Chloe Yarborough <cmyarboro@gmail.com>
2022-06-01 22:25:39 -0400

    added queries.js

---
+++ client/src/utils/queries.js
@@ -0,0 +1,57 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_POSTS = gql`
+query posts($username: String) {
+    posts(username: $username) {
+        _id
+        username
+        postText
+        createdAt
+        likes
+        likeCount
+        comments {
+            _id
+            username
+            commentBody
+            createdAt
+        }
+    }
+}
+`;
+// should we use ID instead of username? 
+export const QUERY_POST = gql`
+query post($username: String!) {
+    post(username: $username) {
+        _id
+        username
+        postText
+        createdAt
+        likes
+        likeCount 
+        comments {
+            _id
+            username
+            commentBody
+            createdAt
+        }
+    }
+}
+`;
+
+export const QUERY_USER =gql`
+query user($username: String!) {
+    user(username: $username) {
+        _id
+        username
+        email 
+        posts {
+            _id
+            postText
+            createdAt
+            likeCount
+        }
+        likedPosts {
+            _id
+        }
+    }
+}`
