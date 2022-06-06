4340c3f961cd1acc4167706cebae65a9d85e3fab
Cherboi <rplatzer98@gmail.com>
2022-06-06 14:24:34 -0400

    drop buttons added

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -1,85 +1,85 @@
 import React from "react";
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
+import { Card } from "react-bootstrap";
 import Button from 'react-bootstrap/Button'
-import ButtonGroup from 'react-bootstrap/ButtonGroup'
+import { Accordion, useAccordionButton } from "react-bootstrap";
 
-
-
-// need to render the page with and acoounts settings with change PFP, change password, and add birthday
-const Account = () => {
-  return (
-    <div className="container mt-5 mb-5">
-      <div className="row">
-        <h1 className="opacity-25 mb-3 text-light">
-          Account <span className="fw-light">Settings</span>
-        </h1>
-      </div>
-      <>
-        <div className="row">
-          <h1 className="opacity-25 mb-3 text-light">
-            Change You're Settings
-          </h1>
-          <ButtonGroup vertical>
-            <Button>Change</Button>
-          </ButtonGroup>
-          <InputGroup className="mb-3">
-            <FormControl
-              placeholder="Change You're Username"
-              aria-label="Username"
-              aria-describedby="basic-addon2"
-            />
-            <Button
-              variant="dark">
-              Submit
-            </Button>
-
-          </InputGroup>
-          <InputGroup className="mb-3">
-            <FormControl
-              placeholder="Change You're Email"
-              aria-label="Email"
-              aria-describedby="basic-addon2"
-            />
-            <Button
-              variant="dark">
-              Submit
-            </Button>
-
-          </InputGroup>
-          <InputGroup className="mb-3">
-            <FormControl
-              placeholder="Change You're Password"
-              aria-label="Password"
-              aria-describedby="basic-addon2"
-            />
-            <Button
-              variant="dark">
-              Submit
-            </Button>
-
-          </InputGroup>
-        </div>
-      </>
-      <div className="row">
-        <h1 className="opacity-25 mb-3 text-light">
-          Change You're Settings
-        </h1>
-        <ButtonGroup vertical>
-          <Button>Button</Button>
-        </ButtonGroup>
-      </div>
-      <div className="row">
-        <h1 className="opacity-25 mb-3 text-light">
-          Change You're Settings
-        </h1>
-        <ButtonGroup vertical>
-          <Button>Button</Button>
-        </ButtonGroup>
-      </div>
-
-    </div >
+function CustomToggle({ children, eventKey }) {
+  const decoratedOnClick = useAccordionButton(eventKey, () =>
+    console.log('totally custom!'),
   );
+
+  return (
+    <button
+      type="button"
+      style={{ backgroundColor: 'pink' }}
+      onClick={decoratedOnClick}
+    >
+      {children}
+    </button>
+  );
+}
+
+function Account() {
+  return (
+    <Accordion defaultActiveKey="0">
+      <Card>
+        <Card.Header>
+          <CustomToggle eventKey="0">
+            Change Your Settings
+            </CustomToggle>
+        </Card.Header>
+        <Accordion.Collapse eventKey="0">
+          <Card.Body>
+            <>
+              <InputGroup className="mb-3">
+                <FormControl
+                  size="lg"
+                  placeholder="Change You're Username"
+                  aria-label="Username"
+                  aria-describedby="basic-addon2"
+                />
+
+              </InputGroup>
+              <InputGroup className="mb-3">
+                <FormControl
+                  size="lg"
+                  placeholder="Change You're Email"
+                  aria-label="Email"
+                  aria-describedby="basic-addon2"
+                />
+                <InputGroup.Text id="basic-addon2">@Email.com</InputGroup.Text>
+              </InputGroup>
+              <InputGroup className="mb-3">
+                <FormControl
+                  size="lg"
+                  placeholder="Change You're Password"
+                  aria-label="Password"
+                  aria-describedby="basic-addon2"
+                />
+              </InputGroup>
+              <Button>
+                Submit
+              </Button>
+            </>
+          </Card.Body>
+        </Accordion.Collapse>
+      </Card>
+      <Card>
+        <Card.Header>
+          <CustomToggle eventKey="1">Update Your Birthday</CustomToggle>
+        </Card.Header>
+        <Accordion.Collapse eventKey="1">
+          <Card.Body>
+
+          </Card.Body>
+        </Accordion.Collapse>
+      </Card>
+    </Accordion>
+  );
+
+  // need to render the page with and acoounts settings with change PFP, change password, and add birthday
 };
 
 export default Account;
