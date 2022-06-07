3cc76b94ac32c99a9bbd42c6299d319005e3b94e
alen <alen.dunn1@gmail.com>
2022-06-06 21:51:37 -0400

    remove unecessary import

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import 'react-calendar/dist/Calendar.css';
+
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
 import { Card } from "react-bootstrap";
